5d633d52e094b239e8f1ac0462abe64e54036442
lucifer950924 <amaiti.9509@gmail.com>
2025-06-17 10:42:16 +0530

    Added_extra_logging_feature_using_snoop_17062025

--- runner.py
+++ runner.py
@@ -4,35 +4,44 @@ from datetime import datetime as dt
 import shutil
 import yaml
 import pyautogui
+import snoop
+import logging
+from functools import wraps
 
-
-with open("runner_config.yml","r") as f:
-    data = yaml.safe_load(f)
-
-config_data = data['config_data']
 
 now = dt.now().strftime("%Y%m%d%H%M%S")
 curr_dir = os.path.join(os.getcwd(),"Results")
 results_dir = os.path.join(curr_dir,f"Results_{now}")
 print(results_dir)
 os.makedirs(results_dir,exist_ok=True)
-with open("results.txt", 'w') as f:
-    try:
-        subprocess.run([config_data['language'],config_data['suite']],stdout=f,stderr=subprocess.STDOUT)
-        sc = pyautogui.screenshot()
+log_file = f"results_{now}.html"
+results_filepath = os.path.join(results_dir,log_file)
+snoop.install(out=open(results_filepath,"w"))
 
-        screenshot_directory = os.path.join(results_dir,"Screenshots")
-        os.makedirs(screenshot_directory)
-        sc_filepath = os.path.join(screenshot_directory,f"sc_{now}.png")
-        sc.save(sc_filepath)
-        f.write(f"{config_data['suite']} ran successfully")
-    except:
-        f.write(f"{config_data['suite']} ran unsuccessfully")
+@snoop
+def runner():
+    with open("runner_config.yml","r") as f:
+        data = yaml.safe_load(f)
 
-results_path = os.path.join(results_dir,"results.txt")
-shutil.move("results.txt",results_dir)
+    config_data = data['config_data']
 
-def take_screenshot(path=sc_filepath):
-    sc = pyautogui.screenshot()
 
-    sc.save(path)
+    with open("results.txt", 'w') as f:
+        try:
+            subprocess.run([config_data['language'],config_data['suite']],stdout=f,stderr=subprocess.STDOUT)
+            sc = pyautogui.screenshot()
+
+            screenshot_directory = os.path.join(results_dir,"Screenshots")
+            os.makedirs(screenshot_directory)
+            sc_filepath = os.path.join(screenshot_directory,f"sc_{now}.png")
+            sc.save(sc_filepath)
+            f.write(f"{config_data['suite']} ran successfully")
+        except:
+            f.write(f"{config_data['suite']} ran unsuccessfully")
+
+
+    shutil.move("results.txt",results_dir)
+
+
+
+runner()
